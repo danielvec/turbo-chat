1a560ebb34f9d3f75b44d454fee48af595cd510e
danielvec <vechioladaniel@gmail.com>
2023-06-08 20:47:49 -0500

    add messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :rooms
+  root "rooms#index"
+  resources :rooms do
+    resources :messages
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
